e5a689a43991b4b0fec0b9bbcb375f5adf9a4e51
alchemycodess <alchemycodess@gmail.com>
2026-01-13 00:05:20 +0530

    feat: add temperature conversion

--- be-projects/unit-converter/script.js
+++ be-projects/unit-converter/script.js
@@ -34,6 +34,22 @@ app.post("/convertw", (req, res) => {
     `)
 })
 
+app.post("/convertt", (req, res) => {
+    const data = req.body
+    const tempInC = data.value
+    const tempFrom = data.from
+    const tempTo = data.to
+
+    const tempInF = tempInC * (9/5) + 32
+
+    res.send(`
+        <h1>Result of your calculation</h1>
+        <p>Result:${tempInC} ${tempFrom} = ${tempInF} ${tempTo}</p>
+        <a href="http://localhost:5500/be-projects/unit-converter/">Go back</a>   
+    `)
+
+})
+
 app.listen(3000, () => {
   console.log("Server running on port 3000")
 })
