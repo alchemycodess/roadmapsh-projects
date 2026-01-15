d99dcc5f9561926a62aad49158b09aa821ea9b19
alchemycodess <alchemycodess@gmail.com>
2026-01-15 23:43:45 +0530

    feat: add Redis caching and rate limiting to weather API endpoint
    Signed-off-by: alchemycodess <alchemycodess@gmail.com>

--- be-projects/weather-api/script.js
+++ be-projects/weather-api/script.js
@@ -1,10 +1,27 @@
 import axios from "axios";
-import express from "express"
+import express, { response } from "express"
 import dotenv from "dotenv"
+import rateLimit from "express-rate-limit"
+import { createClient } from "redis";
 
 dotenv.config()
 const app = express()
 const port = 3000;
+
+const redisClient = createClient()
+
+redisClient.on("error", (err) => {
+    console.log("Redis error", err)
+})
+
+await redisClient.connect()
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+})
+
+app.use(limiter)
 
 app.get("/weather", async (req, res) => {
     const city = req.query.city
@@ -13,11 +30,31 @@ app.get("/weather", async (req, res) => {
         return res.status(400).json({message: "City is required."})
     }
 
+    const cacheKey = `weather:${city}`
+    const cacheData = await redisClient.get(cacheKey)
+
+    if(cacheData) {
+        return res.json(JSON.parse(cacheData))
+    }
+
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${process.env.WEATHER_API_KEY}&unitGroup=metric`
     
     try {
+        console.log("FROM API")
         const response = await axios.get(url)
-        res.json({ city, temperature: response.data.currentConditions.temp })
+
+        const dataToCache = {
+            city,
+            temperature: response.data.currentConditions.temp
+        }
+
+        await redisClient.set(
+            cacheKey,
+            JSON.stringify(dataToCache),
+            {EX: 60 * 60 * 12}
+        )
+        
+        return res.json(dataToCache)
     } catch (error) {
         return res.status(500).json({message: "Failed to fetch weather data"})
     }
